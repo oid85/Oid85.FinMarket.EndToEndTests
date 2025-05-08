fdea5dea784e5343e358fbc690f3a26f6d594c9b
oid85 <oid85@mail.ru>
2025-05-08 22:27:11 +0700

    .

--- modules/api_tests.py
+++ modules/api_tests.py
@@ -14,7 +14,7 @@ def api_list_tickers(url):
     assert len(result) > 0
 
 
-def api_report(url, tickerList):
+def api_report_datarange_tickerlist(url, tickerList):
     # arrange
     toDate = datetime.now()
     fromDate = toDate - timedelta(days=7)
@@ -39,3 +39,70 @@ def api_report(url, tickerList):
     assert len(title) > 0
     assert len(header) > 0
     assert len(data) > 0
+
+
+def api_report_tickerlist(url, tickerList):
+    # arrange
+    body = {
+        "tickerList": tickerList
+    }
+
+    # act
+    response = requests.post(url, json=body)
+    response_json = response.json()
+    result = response_json["result"]
+
+    title = result["title"]
+    header = result["header"]
+    data = result["data"]
+
+    # assert
+    assert response.status_code == 200
+    assert len(title) > 0
+    assert len(header) > 0
+    assert len(data) > 0
+
+
+def api_diagram_close_prices(url, tickerList):
+    # arrange
+    toDate = datetime.now()
+    fromDate = toDate - timedelta(days=7)
+
+    body = {
+        "from": fromDate.strftime('%Y-%m-%d'),
+        "to": toDate.strftime('%Y-%m-%d'),
+        "tickerList": tickerList
+    }
+
+    # act
+    response = requests.post(url, json=body)
+    response_json = response.json()
+    result = response_json["result"]
+
+    title = result["title"]
+    data = result["data"]
+
+    # assert
+    assert response.status_code == 200
+    assert len(title) > 0
+    assert len(data) > 0
+
+
+def api_diagram_multiplicators(url, tickerList):
+    # arrange
+    body = {
+        "tickerList": tickerList
+    }
+
+    # act
+    response = requests.post(url, json=body)
+    response_json = response.json()
+    result = response_json["result"]
+
+    title = result["title"]
+    series = result["series"]
+
+    # assert
+    assert response.status_code == 200
+    assert len(title) > 0
+    assert len(series) > 0
--- test_api_shares.py
+++ test_api_shares.py
@@ -1,5 +1,5 @@
 import config
-from modules.api_tests import api_list_tickers, api_report
+from modules.api_tests import api_list_tickers, api_report_datarange_tickerlist, api_report_tickerlist, api_diagram_close_prices, api_diagram_multiplicators
 
 
 def test_api_shares_watch_list_tickers():
@@ -7,4 +7,76 @@ def test_api_shares_watch_list_tickers():
 
 
 def test_api_shares_report_aggregated_analyse():
-    api_report(f'{config.api_url}/api/shares/report/aggregated-analyse', 'shares/watchlist')
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/aggregated-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_supertrend_analyse():
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/supertrend-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_candle_sequence_analyse():
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/candle-sequence-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_candle_volume_analyse():
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/candle-volume-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_rsi_analyse():
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/rsi-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_atr_analyse():
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/atr-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_donchian_analyse():
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/donchian-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_yield_ltm_analyse():
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/yield-ltm-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_drawdown_from_maximum_analyse():
+    api_report_datarange_tickerlist(f'{config.api_url}/api/shares/report/drawdown-from-maximum-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_dividend_analyse():
+    api_report_tickerlist(f'{config.api_url}/api/shares/report/dividend-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_multiplicator_analyse():
+    api_report_tickerlist(f'{config.api_url}/api/shares/report/multiplicator-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_forecast_target_analyse():
+    api_report_tickerlist(f'{config.api_url}/api/shares/report/forecast-target-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_forecast_consensus_analyse():
+    api_report_tickerlist(f'{config.api_url}/api/shares/report/forecast-consensus-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_active_market_events_analyse():
+    api_report_tickerlist(f'{config.api_url}/api/shares/report/active-market-events-analyse', 'shares/watchlist')
+
+
+def test_api_shares_report_asset_report_events():
+    api_report_tickerlist(f'{config.api_url}/api/shares/report/asset-report-events', 'shares/watchlist')
+
+
+def test_api_shares_report_fear_greed_index():
+    api_report_tickerlist(f'{config.api_url}/api/shares/diagram/fear-greed-index', 'shares/watchlist')
+
+
+def test_api_shares_diagram_daily_close_prices():
+    api_diagram_close_prices(f'{config.api_url}/api/shares/diagram/daily-close-prices', 'shares/watchlist')
+
+
+def test_api_shares_diagram_five_minutes_close_prices():
+    api_diagram_close_prices(f'{config.api_url}/api/shares/diagram/five-minutes-close-prices', 'shares/watchlist')
+
+
+def test_api_shares_diagram_multiplicators_mcap_pe_netdebtebitda():
+    api_diagram_multiplicators(f'{config.api_url}/api/shares/report/multiplicators-mcap-pe-netdebtebitda', 'shares/watchlist')
